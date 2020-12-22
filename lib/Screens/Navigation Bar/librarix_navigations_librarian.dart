8fb61164b49095b2be10670b358d3831b7dda9bc
Leebecky <leebecky06@gmail.com>
2020-12-22 10:05:29 +0800

    Revert "Revert "Revert "Merge branch 'main' into Becky"""
    This reverts commit d549ff1525b1f862b756336690ca2cf33e821174.

--- lib/Screens/Navigation Bar/librarix_navigations_librarian.dart
+++ lib/Screens/Navigation Bar/librarix_navigations_librarian.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:librarix/Screens/borrow_book_scanner.dart';
 import '../test.dart';
-import '../Staff/booking_records.dart';
 import '../catalogue_view.dart';
-import '../Staff/book_management.dart';
 import 'package:librarix/config.dart';
 import '../booking_maker.dart';
-
 
 class LibrarianHome extends StatefulWidget {
   @override
@@ -71,18 +67,7 @@ class _LibrarianHomeState extends State<LibrarianHome> {
                 trailing: new Icon(Icons.book_rounded),
                 onTap: () {
                   //Navigator.of(context).pop();
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return BookingRecords();
-                  }));
-                }),
-            new ListTile(
-                title: new Text("Book Management"),
-                trailing: new Icon(Icons.book_online),
-                onTap: () {
-                  //Navigator.of(context).pop();
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return BookManagement();
-                  }));
+                  //Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Page("Second Page")));
                 }),
             new ListTile(
                 title: new Text("Fines Management"),
