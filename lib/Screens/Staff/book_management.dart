d75e6273d49d432df02393efe72361af753b4802
Leebecky <leebecky06@gmail.com>
2020-12-22 10:05:55 +0800

    Revert "Revert "Revert "Revert "Merge branch 'main' into Becky""""
    This reverts commit 8fb61164b49095b2be10670b358d3831b7dda9bc.

--- lib/Screens/Staff/book_management.dart
+++ lib/Screens/Staff/book_management.dart
@@ -1,66 +1,32 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:librarix/Models/librarian.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-
-// class BookCatelogueListItem {
-//   String bookAuthor;
-//   String bookBarcode;
-//   String bookDesc;
-//   String bookGenre;
-//   String bookISBN;
-//   String bookImageURL;
-//   String bookPublisherDate;
-//   String bookPublisher;
-//   String bookTitle;
-//   int bookStock;
-
-//   BookCatelogueListItem(
-//       {this.bookAuthor,
-//       this.bookBarcode,
-//       this.bookDesc,
-//       this.bookGenre,
-//       this.bookISBN,
-//       this.bookImageURL,
-//       this.bookPublisher,
-//       this.bookPublisherDate,
-//       this.bookStock,
-//       this.bookTitle});
-// }
+import 'package:librarix/Models/book.dart';
 
 class BookManagement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add Book Management"),
+        title: Text("Book Management"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return AddBook_BookManagement();
+          }));
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
-class BookManagemenListView extends StatelessWidget {
+class AddBook_BookManagement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder(
-          future: getLibrarian(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            print(snapshot.data);
-            if (snapshot.data == null) {
-              return Container(child: Center(child: Text("Loading ... ")));
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(snapshot.data[index].name),
-                    subtitle: Text(snapshot.data[index].email),
-                  );
-                },
-              );
-            }
-          }),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Book"),
+      ),
     );
   }
 }
