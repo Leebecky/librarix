70eb6897e667682b192c77077d9c90bf6b73ff78
Leebecky <leebecky06@gmail.com>
2021-01-04 19:58:53 +0800

    Resolved merging errors
    Renamed Fines in Fines_view to FinesDisplay

--- lib/Screens/Navigation_Bar/librarix_navigations.dart
+++ lib/Screens/Navigation_Bar/librarix_navigations.dart
@@ -96,7 +96,7 @@ class _HomeState extends State<Home> {
                 trailing: Icon(Icons.monetization_on_rounded),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Fines();
+                    return FinesDisplay();
                   }));
                 }),
             ListTile(
