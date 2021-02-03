2551edb73cc4699d357abc9c90d2ad1005c738a4
Leebecky <leebecky06@gmail.com>
2021-02-03 20:21:05 +0800

    Added actionAlertDialog
    New alertDialog widget that accepts onPressed function calls

--- lib/Custom_Widget/custom_alert_dialog.dart
+++ lib/Custom_Widget/custom_alert_dialog.dart
@@ -33,3 +33,34 @@ customAlertDialog(BuildContext context,
                 child: Text("Close"))
           ]));
 }
+
+actionAlertDialog(BuildContext context,
+    {String title = "",
+    String content = "",
+    navigateHome = false,
+    Function onPressed}) {
+  return showDialog(
+      context: context,
+      child: AlertDialog(title: Text(title), content: Text(content), actions: [
+        FlatButton(
+          onPressed: onPressed,
+          child: Text("Yes"),
+        ),
+        FlatButton(
+            onPressed: () async {
+              if (navigateHome) {
+                //^ When true, returns to the Book Catalogue page
+                ActiveUser myUser = await myActiveUser(
+                    docId: FirebaseAuth.instance.currentUser.uid);
+                (myUser.role == "Admin")
+                    ? Navigator.popAndPushNamed(context, "/adminHome")
+                    : (myUser.role == "Librarian")
+                        ? Navigator.popAndPushNamed(context, "/librarianHome")
+                        : Navigator.popAndPushNamed(context, "/home");
+              } else {
+                Get.back();
+              }
+            },
+            child: Text("No"))
+      ]));
+}
--- lib/Screens/Staff/fines_management.dart
+++ lib/Screens/Staff/fines_management.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:librarix/Custom_Widget/custom_alert_dialog.dart';
 import 'package:librarix/Models/fines.dart';
 
 class FinesManagement extends StatefulWidget {
@@ -27,69 +28,55 @@ class _FinesManagementState extends State<FinesManagement> {
                 itemBuilder: (_, index) {
                   return Column(
                     children: <Widget>[
-                      Container(
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(14.0),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 8.0, bottom: 4.0),
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        activeFines[index].userId,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 22),
-                                      ),
-                                      Spacer(),
-                                      Column(
-                                        children: actionButtons(index),
-                                      ),
-                                    ],
-                                  ),
+                      Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 8.0, bottom: 4.0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      activeFines[index].userId,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 22),
+                                    ),
+                                    Spacer(),
+                                    Column(
+                                      children: actionButtons(index),
+                                    ),
+                                  ],
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 8.0, bottom: 4.0),
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        "RM " + activeFines[index].total,
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                    ],
-                                  ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 8.0, bottom: 4.0),
+                                child: Text(
+                                  "RM " + activeFines[index].total,
+                                  style: TextStyle(fontSize: 18),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 8.0, bottom: 4.0),
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        "Issue Date: " +
-                                            activeFines[index].issueDate,
-                                        style: TextStyle(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 8.0, bottom: 4.0),
+                                child: Text(
+                                  "Issue Date: " + activeFines[index].issueDate,
+                                  style: TextStyle(fontSize: 16),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 8.0, bottom: 4.0),
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        "Reason : " + activeFines[index].reason,
-                                        style: TextStyle(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 8.0, bottom: 4.0),
+                                child: Text(
+                                  "Reason : " + activeFines[index].reason,
+                                  style: TextStyle(fontSize: 16),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
                       )
@@ -110,63 +97,17 @@ class _FinesManagementState extends State<FinesManagement> {
     buttons.add(IconButton(
       icon: Icon(Icons.update),
       onPressed: () {
-        return showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text(
-                  'Fines Management',
-                ),
-                content: SingleChildScrollView(
-                  child: ListBody(
-                    children: <Widget>[Text("Paying fines?")],
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text("Yes"),
-                    onPressed: () async {
-                      updateFinesPaidStatus(activeFines[index].finesId);
-                      return showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Fines Management'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text('Fines payment received'),
-                                  ],
-                                ),
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: Text('OK'),
-                                  onPressed: () async {
-                                    //   Navigator.push(context,
-                                    //       MaterialPageRoute(builder: (context) {
-                                    //     return FinesManagement();
-                                    //   }));
-                                    Navigator.of(context).pop();
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          });
-                    },
-                  ),
-                  TextButton(
-                    child: Text("No"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              );
-            });
+        return actionAlertDialog(
+          context,
+          title: "Fines Management",
+          content: "Paying fines?",
+          onPressed: () async {
+            updateFinesPaidStatus(activeFines[index].finesId);
+            Navigator.of(context).pop();
+            return customAlertDialog(context,
+                title: "Fines Management", content: "Fines payment received");
+          },
+        );
       },
     ));
     return buttons;
